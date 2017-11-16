d39beb1102b12a3c1970feee80221e482151a6ee
Anthony Kelley <anthonymkelley@gmail.com>
2017-11-16 09:22:10 -0700

    initial upload

---
+++ main.js
@@ -0,0 +1,88 @@
+document.getElementById('tile1').addEventListener("click", myFunction1);
+function myFunction1() {
+  document.getElementById(this.id).innerHTML = "JavaScript";
+  setTimeout(function() {
+    
+  }, 1000);
+}
+
+document.getElementById('tile2').addEventListener("click", myFunction2);
+function myFunction2() {
+  document.getElementById(this.id).innerHTML = "Ruby";
+}
+
+document.getElementById('tile3').addEventListener("click", myFunction3);
+function myFunction3() {
+  document.getElementById(this.id).innerHTML = "Python";
+}
+
+document.getElementById('tile4').addEventListener("click", myFunction4);
+function myFunction4() {
+  document.getElementById(this.id).innerHTML = "C";
+}
+
+document.getElementById('tile5').addEventListener("click", myFunction5);
+function myFunction5() {
+  document.getElementById(this.id).innerHTML = "Slack";
+}
+
+document.getElementById('tile6').addEventListener("click", myFunction6);
+function myFunction6() {
+  document.getElementById(this.id).innerHTML = "Terminal";
+}
+
+document.getElementById('tile7').addEventListener("click", myFunction7);
+function myFunction7() {
+  document.getElementById(this.id).innerHTML = "vim";
+}
+
+document.getElementById('tile8').addEventListener("click", myFunction8);
+function myFunction8() {
+  document.getElementById(this.id).innerHTML = "Slack";
+}
+
+document.getElementById('tile9').addEventListener("click", myFunction9);
+function myFunction9() {
+  document.getElementById(this.id).innerHTML = "SQL";
+}
+
+document.getElementById('tile10').addEventListener("click", myFunction10);
+function myFunction10() {
+  document.getElementById(this.id).innerHTML = "Python";
+}
+
+document.getElementById('tile11').addEventListener("click", myFunction11);
+function myFunction11() {
+  document.getElementById(this.id).innerHTML = "C";
+}
+
+document.getElementById('tile12').addEventListener("click", myFunction12);
+function myFunction12() {
+  document.getElementById(this.id).innerHTML = "Terminal";
+}
+
+document.getElementById('tile13').addEventListener("click", myFunction13);
+function myFunction13() {
+  document.getElementById(this.id).innerHTML = "JavaScript";
+}
+
+document.getElementById('tile14').addEventListener("click", myFunction14);
+function myFunction14() {
+  document.getElementById(this.id).innerHTML = "SQL";
+}
+
+document.getElementById('tile15').addEventListener("click", myFunction15);
+function myFunction15() {
+  document.getElementById(this.id).innerHTML = "vim";
+}
+
+document.getElementById('tile16').addEventListener("click", myFunction16);
+function myFunction16() {
+  document.getElementById(this.id).innerHTML = "Ruby";
+}
+
+
+// $(document ).ready(function(){
+//   $(".col s3").click(function(){
+//     $("p").hide();
+// });
